33faa4707e6b4aa5efeee7dfea6b921e480ac020
Ricardo Golding <goldingra@gmail.com>
2025-02-18 15:52:03 +0100

    Added new columns to main dataframe and updated functions file

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -15,3 +15,12 @@ def save_to_csv(df: pd.DataFrame, csv_name: str):
     full_path = directory_path + csv_name
     df.to_csv(full_path, index=False)
     print(f"DataFrame successfully saved to {full_path}")
+
+
+def backward_steps_for_client(steps , client, variation)->dict:
+    count_backward=0
+    for i in range (1, len(steps)):
+         if (steps[i-1] - steps[i] != 1):
+             count_backward=count_backward+1
+    total_steps=len(steps)-1
+    return {client:count_backward/total_steps if total_steps > 0 else 0}
